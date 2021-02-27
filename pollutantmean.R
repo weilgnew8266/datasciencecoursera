b8ef9eec8f2fbf897ef1b9d5a09b043626d3431e
weilgnew <whliew8266@gmail.com>
2021-02-28 01:56:56 +0800

    The function source for the quiz in course "R programming"_week 2

---
+++ pollutantmean.R
@@ -0,0 +1,12 @@
+pollutantmean <- function(directory, pollutant, id = 1:322){
+  ## 'directory' is a character vector of length 1 indicating the 
+  ## location of the CSV files
+  setwd(directory)
+  
+  
+  
+  
+  
+  ## set the directory back to original level
+  setwd("..")
+}
