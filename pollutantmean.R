ff9d3e00abefa2e064d91edea3fb872a7b78f76e
weilgnew <whliew8266@gmail.com>
2021-02-28 02:33:52 +0800

    Add in the skeleton of the function "pollutantmean"

--- pollutantmean.R
+++ pollutantmean.R
@@ -3,10 +3,16 @@ pollutantmean <- function(directory, pollutant, id = 1:322){
   ## location of the CSV files
   setwd(directory)
   
+  ## read the data from the csv file
+  read.csv()
   
-  
-  
+  ## calculate the mean value
+  mean_value = mean(x, trim = 0, na.rm = FALSE, ...)
   
   ## set the directory back to original level
   setwd("..")
+  
+  return (mean_value)
+  
+  
 }
